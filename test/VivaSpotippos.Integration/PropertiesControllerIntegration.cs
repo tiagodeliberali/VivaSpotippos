8748ac311eb21ea46fc967d019578fb4f083fd1b
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-02-09 00:44:43 -0200

    Added integration test to get by id

--- test/VivaSpotippos.Integration/PropertiesControllerIntegration.cs
+++ test/VivaSpotippos.Integration/PropertiesControllerIntegration.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
+using VivaSpotippos.Model;
 using VivaSpotippos.Model.RestEntities;
 using VivaSpotippos.Test;
 using Xunit;
@@ -74,6 +75,75 @@ namespace VivaSpotippos.Integration
                     Assert.Null(result.CreatedProperty);
                 }
             }
+        }
+
+        [Fact]
+        public async Task GetSuccessful()
+        {
+            // Arrange
+            using (var host = new TestServer(GetWebHostBuilder()))
+            {
+                using (var client = host.CreateClient())
+                {
+                    client.DefaultRequestHeaders
+                        .Accept
+                        .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    PropertyPostResponse propertyResult = await CreateProperty(client);
+
+                    // Act
+                    var response = await client.GetAsync(string.Format("properties/{0}", propertyResult.CreatedProperty.id));
+
+                    var result = JsonConvert.DeserializeObject<Property>(await response.Content.ReadAsStringAsync());
+
+                    // Assert
+                    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+                    Assert.NotNull(result);
+                    Assert.Equal(propertyResult.CreatedProperty.id, result.id);
+                    Assert.Contains("Scavy", result.provinces);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task GetInvalidId()
+        {
+            // Arrange
+            using (var host = new TestServer(GetWebHostBuilder()))
+            {
+                using (var client = host.CreateClient())
+                {
+                    client.DefaultRequestHeaders
+                        .Accept
+                        .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    int invalidId = 123;
+
+                    // Act
+                    var response = await client.GetAsync(string.Format("properties/{0}", invalidId));
+
+                    var result = JsonConvert.DeserializeObject<Property>(await response.Content.ReadAsStringAsync());
+
+                    // Assert
+                    Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                    Assert.Null(result);
+                }
+            }
+        }
+
+        private static async Task<PropertyPostResponse> CreateProperty(HttpClient client)
+        {
+            var requestData = DemoData.ValidPostRequest;
+
+            var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("properties", content);
+
+            var result = JsonConvert.DeserializeObject<PropertyPostResponse>(await response.Content.ReadAsStringAsync());
+
+            return result;
         }
 
         private static IWebHostBuilder GetWebHostBuilder()
