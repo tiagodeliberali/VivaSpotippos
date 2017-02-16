b82f4c073b40f1c2834dcfe9bf705c9cb7cf0bfe
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-02-16 12:45:11 -0200

    Added empty performance test solution

---
+++ test/VivaSpotippos.Performance/MappingPerformance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VivaSpotippos.Performance
+{
+    public class MappingPerformance
+    {
+        [Theory]
+        public void CheckPerformance()
+        {
+
+        }
+    }
+}
